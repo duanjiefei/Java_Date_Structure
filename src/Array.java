d13eafe6cbb51bdb1943907371cac4837e407447
duanjiefei <jiefeiduan@163.com>
2018-09-21 00:11:02 +0800

    增加向数组指定位置添加元素的方法

--- src/Array.java
+++ src/Array.java
@@ -46,10 +46,40 @@ public class Array {
      * @param e
      */
     public void addLast(int e){
-        if (size == data.length){
+/*        if (size == data.length){
             throw new IllegalStateException("add last failed,the array is full");
         }
         data[size] = e;
+        size++;*/
+        add(size,e);
+    }
+
+    /**
+     * 向数组的头部添加元素
+     * @param e
+     */
+    public void addFirst(int e){
+        add(0,e);
+    }
+
+    /**
+     * 向数组指定的index位置插入元素
+     * @param index
+     * @param e
+     */
+    public void add(int index, int e){
+        if (size == data.length){
+            throw  new IllegalArgumentException("Add failed,the array is full");
+        }
+
+        if (index < 0 || index > data.length){
+            throw  new IllegalArgumentException("index is not legal ,add failed");
+        }
+
+        for (int i = size - 1;i >= index; i--){
+            data[i+1] = data[i];//将数组的元素依次后移
+        }
+        data[index] = e;
         size++;
     }
 }
